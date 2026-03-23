df8353230e7e6f1c8fad8b4327e36f678d6811ce
abbasmgaliyakot-dot <abbasmgaliyakot@gmail.com>
2026-03-23 04:16:29 +0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter, HTTPException, Depends, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 import logging
